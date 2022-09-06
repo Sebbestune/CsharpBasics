96eff0864a7c1917c450dfac2b42f109a04a96b4
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-09-06 12:49:06 +0300

    En förändring som har skett i en branch!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Det här är kul!");
+
+Console.WriteLine("Det här är skapat i en branch!");
